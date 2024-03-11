946ee2d40ffcb4768bf89538884ac70873f5ee43
Danil <damayorov93@gmail.com>
2024-03-11 19:26:23 +0500

    edit_lesson_10

--- script.js
+++ script.js
@@ -3,24 +3,24 @@
 const bookShelf = document.querySelector('.books');
 const booksArr = document.querySelectorAll('.book');
 
-// 1
+// 1 Восстановление порядка книг
 bookShelf.prepend(booksArr[1]);
 bookShelf.append(booksArr[2]);
 booksArr[3].before(booksArr[4]);
 
-// 2
+// 2 Замена картинки заднего фона
 document.body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
 
-// 3
+// 3 Исправление заголовка в книге 3
 let newTitle = booksArr[4].querySelector('a').textContent.replace("Пропопипы", "Прототипы");
 booksArr[4].querySelector('a').textContent = newTitle;
 
-// 4
+// 4 Удаление рекламы
 const banner = document.querySelector('.adv');
 
 banner.remove();
 
-// 5
+// 5 Восстановление порядка глав во второй и пятой книгах
 const secondBook = booksArr[0].querySelectorAll('li');
 const fifthBook = booksArr[5].querySelectorAll('li');
 const sixthBook = booksArr[2].querySelectorAll('li');
@@ -32,7 +32,6 @@ fifthBook[fifthBook.length - 1].before(fifthBook[5], fifthBook[8]);
 fifthBook[2].replaceWith(fifthBook[9]);
 fifthBook[4].after(fifthBook[2]);
 
-// 6
-const newBook = document.createElement('li').textContent = "Глава 8: За пределами ES6";
+// 6 добавление главы
+sixthBook[sixthBook.length - 1].insertAdjacentHTML('beforebegin', `<li>Глава 8: За пределами ES6</li>`);
 
-sixthBook[sixthBook.length - 1].before(newBook);
