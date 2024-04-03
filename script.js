efa40a514baa0da389e5f155473a783058fb1790
damayorov93@gmail.com <damayorov93@gmail.com>
2024-04-03 18:42:23 +0500

    before lesson 15

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const bookShelf = document.querySelector('.books');
+/* const bookShelf = document.querySelector('.books');
 const booksArr = document.querySelectorAll('.book');
 
 // 1
@@ -35,4 +35,32 @@ fifthBook[4].after(fifthBook[2]);
 // 6
 const newBook = document.createElement('li').textContent = "Глава 8: За пределами ES6";
 
-sixthBook[sixthBook.length - 1].before(newBook);
+sixthBook[sixthBook.length - 1].before(newBook); */
+
+
+const addButton = document.querySelector('button');
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+
+
+const liAdd = function(elem) {
+  if (elem.value) {
+    list.insertAdjacentHTML('beforeend', `<li>${elem.value}</li>`);
+    elem.value = "";
+  }
+    return console.log('Пустой инпут');
+
+
+};
+let boundFunc = liAdd.bind(null, input);
+
+addButton.addEventListener('click', boundFunc);
+// решил использовать bind для передачи аргумента в функцию в слушателе, но не до конца понятно как правильно его использовать
+// да и вообще можно было обойтись без этого а сразу в условие писать input.value
+
+
+
+
+
+
+
